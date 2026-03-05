bfc76091b4f806d5360ac5fe197055a5b08ea392
LuisAlejandroAbreo <alejandro.abreo39@gmail.com>
2026-03-05 15:52:00 -0500

    fix: se corrige la funcion listar() para mostrar las empanadas

--- Listar.py
+++ Listar.py
@@ -2,18 +2,19 @@ from utils import cargar_empanadas
 
 def listar_empanadas():
     empanadas = cargar_empanadas()
+
     if not empanadas:
         print("No hay empanadas registradas.")
         return
 
     print("\n*** EMPANADAS REGISTRADAS ***")
 
-    for i, e in enumerate(empanadas):
+    for i, e in enumerate(empanadas, 1):
 
         print(f"\nEmpanada {i}")
         print(f"Nombre: {e['nombre']}")
         print(f"Precio: {e['precio']}")
         print(f"Ingredientes: {e['ingredientes']}")
-        print(f"Estado {e['disponible']}")
+        print(f"Estado: {e['disponible']}")
 
 listar_empanadas()
